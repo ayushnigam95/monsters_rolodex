18da7c1a9c3d95501430c3c6a4c25d81f9c94471
Ayush <ayush.nigam95@gmail.com>
2020-04-06 04:19:27 +0530

    class component done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component }from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super();
+
+    // this.state comes from Component class
+    this.state = {
+      string: "Ayush"
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {/* this {} is allowed by JSX
+            it means that any thing between curly braces is javascript */}
+            Class Component { this.state.string }
+          </p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          {/* this.setState() comes from Component Class */}
+          <button onClick={() => this.setState({string: "Hello Ayush"})}>
+            My button
+          </button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
