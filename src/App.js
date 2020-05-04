e45221bc70c47bd0b392a8674cfc88c9519d0ebf
Ayush <ayush.nigam95@gmail.com>
2020-05-04 09:42:56 +0530

    last save

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
-import React, { Component }from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component{
-
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
 
     // this.state comes from Component class
     this.state = {
-      string: "Ayush"
-    }
+      monsters: [
+        {
+          name: "Frankstein",
+          id: "1"
+        },
+        {
+          name: "batman",
+          id: "2"
+        },
+        {
+          name: "superman",
+          id: "3"
+        },
+      ],
+    };
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {/* this {} is allowed by JSX
-            it means that any thing between curly braces is javascript */}
-            Class Component { this.state.string }
-          </p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          {/* this.setState() comes from Component Class */}
-          <button onClick={() => this.setState({string: "Hello Ayush"})}>
-            My button
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <h1 key= {monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
